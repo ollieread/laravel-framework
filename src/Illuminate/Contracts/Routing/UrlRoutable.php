ad074289eb1abcac1896be56879544969f772ca9
Ollie <code@ollie.codes>
2023-12-19 10:15:00 +0000

    Expand UrlRoutable return types to include object, and not just Model

--- src/Illuminate/Contracts/Routing/UrlRoutable.php
+++ src/Illuminate/Contracts/Routing/UrlRoutable.php
@@ -5,35 +5,35 @@ namespace Illuminate\Contracts\Routing;
 interface UrlRoutable
 {
     /**
-     * Get the value of the model's route key.
+     * Get the value of the object's route key.
      *
      * @return mixed
      */
     public function getRouteKey();
 
     /**
-     * Get the route key for the model.
+     * Get the route key for the object.
      *
      * @return string
      */
     public function getRouteKeyName();
 
     /**
-     * Retrieve the model for a bound value.
+     * Retrieve the object for a bound value.
      *
      * @param  mixed  $value
      * @param  string|null  $field
-     * @return \Illuminate\Database\Eloquent\Model|null
+     * @return \Illuminate\Database\Eloquent\Model|object|null
      */
     public function resolveRouteBinding($value, $field = null);
 
     /**
-     * Retrieve the child model for a bound value.
+     * Retrieve the child object for a bound value.
      *
      * @param  string  $childType
      * @param  mixed  $value
      * @param  string|null  $field
-     * @return \Illuminate\Database\Eloquent\Model|null
+     * @return \Illuminate\Database\Eloquent\Model|object|null
      */
     public function resolveChildRouteBinding($childType, $value, $field);
 }
